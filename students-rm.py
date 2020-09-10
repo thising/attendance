36b25edbc0dc64d12eac906ac52f87101e27cde1
thising <thising@gmail.com>
2020-09-10 15:22:57 +0800

    2020.09 新学期名单变更

---
+++ students-rm.py
@@ -0,0 +1,22 @@
+from django.db import models
+from manage.models import *
+
+with open("./delete.data", "r") as f:
+    buf = f.read()
+    students = buf.split("\n")
+    for s in students:
+        student = Student.objects.get(number = s)
+        print("***Remove ", student)
+        
+        records = student.report_set.all()
+        for i in records:
+            print("- Remove ", i)
+            i.delete()
+
+        records = student.summarycount_set.all()
+        for i in records:
+            print("- Remove ", i)
+            i.delete()
+        
+        student.delete()
+        print("***Finish")
